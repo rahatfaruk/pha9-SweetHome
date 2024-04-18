93e432ffeca9a4ef02f7854d2daffbae3a55b2d8
rahatfaruk <rahatfaruk99@gmail.com>
2024-04-18 06:41:16 +0600

    Homepage loading worked

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,8 @@
+import { useContext } from 'react';
 import {useLoaderData} from 'react-router-dom';
 import {Helmet} from 'react-helmet';
+import { AuthContext } from '../../context/AuthProvider';
+import Loading from '../../comps/Loading';
 import Banner from "./Banner";
 import Estates from './Estates';
 import Agents from './Agents';
@@ -7,7 +10,11 @@ import WhyUs from './WhyUs';
 
 function Home() {
   const estates = useLoaderData()
+  const {loading} = useContext(AuthContext)
 
+  if (loading) {
+    return <Loading />
+  }
   return (  
     <div>
       <Helmet>
